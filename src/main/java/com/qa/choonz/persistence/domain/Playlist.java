bf266d6f09a8e698efd9db3b91c51d6f1f16eb1a
SehunB-QA <sbabatunde@qa.com>
2021-03-09 15:03:57 +0000

    Added a user domain for the user account feature. Added a ManyToOne playlist to user

--- src/main/java/com/qa/choonz/persistence/domain/Playlist.java
+++ src/main/java/com/qa/choonz/persistence/domain/Playlist.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -37,6 +38,9 @@ public class Playlist {
 
     @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL)
     private List<Track> tracks;
+    
+	@ManyToOne
+	private Playlist playlist = null;
 
     public Playlist() {
         super();
