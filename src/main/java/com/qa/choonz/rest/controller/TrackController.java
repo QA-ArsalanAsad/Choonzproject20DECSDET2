70daf55328f650dc7532d240281a0a390e7b55b7
CGuthrieQA <CGuthrie@QA.com>
2021-03-11 07:39:56 +0000

    refactored track controller

--- src/main/java/com/qa/choonz/rest/controller/TrackController.java
+++ src/main/java/com/qa/choonz/rest/controller/TrackController.java
@@ -27,33 +27,32 @@ public class TrackController {
 	private TrackService service;
 
 	public TrackController(TrackService service) {
-		super();
 		this.service = service;
 	}
 
 	@PostMapping("/create")
 	public ResponseEntity<TrackDTO> create(@RequestBody Track track) {
-		return new ResponseEntity<TrackDTO>(this.service.create(track), HttpStatus.CREATED);
+		return new ResponseEntity<>(this.service.create(track), HttpStatus.CREATED);
 	}
 
 	@GetMapping("/read")
 	public ResponseEntity<List<TrackDTO>> read() {
-		return new ResponseEntity<List<TrackDTO>>(this.service.read(), HttpStatus.OK);
+		return ResponseEntity.ok(this.service.read());
 	}
 
 	@GetMapping("/read/{id}")
 	public ResponseEntity<TrackDTO> read(@PathVariable long id) {
-		return new ResponseEntity<TrackDTO>(this.service.read(id), HttpStatus.OK);
+		return ResponseEntity.ok(this.service.read(id));
 	}
 
 	@PutMapping("/update/{id}")
 	public ResponseEntity<TrackDTO> update(@RequestBody Track track, @PathVariable long id) {
-		return new ResponseEntity<TrackDTO>(this.service.update(track, id), HttpStatus.ACCEPTED);
+		return new ResponseEntity<>(this.service.update(track, id), HttpStatus.ACCEPTED);
 	}
 
 	@DeleteMapping("delete/{id}")
 	public ResponseEntity<TrackDTO> delete(@PathVariable long id) {
 		return this.service.delete(id) ? new ResponseEntity<TrackDTO>(HttpStatus.NO_CONTENT)
-				: new ResponseEntity<TrackDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
