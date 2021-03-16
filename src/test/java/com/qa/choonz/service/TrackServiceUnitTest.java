3f709662ff573935b41e39c206a432a44e3f1f46
RaimondsMezalsQA <RMezals@qa.com>
2021-03-16 14:32:02 +0000

    Completed track service unit tests

--- src/test/java/com/qa/choonz/service/TrackServiceUnitTest.java
+++ src/test/java/com/qa/choonz/service/TrackServiceUnitTest.java
@@ -1,5 +1,154 @@
 package com.qa.choonz.service;
 
-public class TrackServiceUnitTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.qa.choonz.persistence.domain.Album;
+import com.qa.choonz.persistence.domain.Track;
+import com.qa.choonz.persistence.repository.TrackRepository;
+import com.qa.choonz.rest.dto.TrackDTO;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class TrackServiceUnitTest {
+	@MockBean
+	private TrackRepository repo;
+
+	@Autowired
+	private ModelMapper mapper;
+
+	@Autowired
+	private TrackService service;
+
+	private TrackDTO mapToDTO(Track track) {
+		return this.mapper.map(track, TrackDTO.class);
+	}
+
+	private TrackDTO mapToDTO2(List<TrackDTO> list) {
+		return this.mapper.map(list, TrackDTO.class);
+	}
+
+	private TrackDTO mapToDTO3(List<Track> listTrack2) {
+		return this.mapper.map(listTrack2, TrackDTO.class);
+	}
+
+	private final Track A_TEST_1 = new Track(1L, "TestTrack1", 111, "Lyrics");
+	private final Track A_TEST_2 = new Track(2L, "TestTrack2", 222, "Lyrics");
+	private final List<Track> listTrack = List.of(A_TEST_1, A_TEST_2);
+
+	@BeforeEach
+	void beforeEach() {
+
+		A_TEST_1.setAlbum(new Album(1L));
+		A_TEST_2.setAlbum(new Album(1L));
+
+	}
+
+	@Test
+	void testCreate() throws Exception {
+		// GIVEN
+		TrackDTO testTrackDto = mapToDTO(A_TEST_1);
+
+		// AND
+		when(repo.save(A_TEST_1)).thenReturn(A_TEST_1);
+
+		// WHEN
+		TrackDTO result = service.create(testTrackDto, 1L);
+		assertEquals(testTrackDto, result);
+
+		// THEN
+		verify(this.repo, atLeastOnce()).save(A_TEST_1);
+
+	}
+
+	@Test
+	void testReadAll() throws Exception {
+		// GIVEN
+
+		// AND
+		when(repo.findAll()).thenReturn(listTrack);
+
+		// WHEN
+		TrackDTO result = mapToDTO2(service.read());
+		assertEquals(mapToDTO3(listTrack), result);
+
+		// THEN
+		verify(this.repo, atLeastOnce()).findAll();
+	}
+
+	@Test
+	void testReadByID() throws Exception {
+		// GIVEN
+		Optional<Track> testTrack = Optional.of(A_TEST_1);
+		TrackDTO testTrackDto = mapToDTO(A_TEST_1);
+
+		// AND
+		when(repo.findById(1L)).thenReturn(testTrack);
+
+		// WHEN
+		TrackDTO result = service.read(A_TEST_1.getId());
+		assertEquals(testTrackDto, result);
+		assertEquals(true, testTrack.isPresent());
+
+		// THEN
+		verify(this.repo, atLeastOnce()).findById(A_TEST_1.getId());
+
+	}
+
+	@Test
+	void testUpdate() throws Exception {
+		// GIVEN
+		Track testTrack = new Track(1L, "TestTrack1 Updated", 111, "Lyrics Updated");
+		TrackDTO testTrackDto = mapToDTO(testTrack);
+		Optional<Track> testTrackOp = Optional.of(testTrack);
+
+		// AND
+		when(repo.findById(1L)).thenReturn(testTrackOp);
+		when(repo.save(testTrack)).thenReturn(testTrack);
+
+		// WHEN
+		TrackDTO result = service.update(testTrackDto, 1L);
+		assertEquals(testTrackDto, result);
+		assertEquals(true, testTrackOp.isPresent());
+
+		// THEN
+		verify(this.repo, atLeastOnce()).save(testTrack);
+
+	}
+
+	@Test
+	void testDelete() throws Exception {
+
+		Boolean result = service.delete(1L);
+
+		assertEquals(true, result);
+		verify(this.repo, atLeastOnce()).deleteById(A_TEST_1.getId());
+
+	}
+
+	@Test
+	void testDeleteFalse() throws Exception {
+
+		when(repo.existsById(999L)).thenReturn(true);
+
+		Boolean result = service.delete(999L);
+
+		assertEquals(false, result);
+		verify(this.repo, atLeastOnce()).deleteById(999L);
+		verify(this.repo, atLeastOnce()).existsById(999L);
+
+	}
 }
