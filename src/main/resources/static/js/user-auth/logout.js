2043d2680e26dbfacb1e46c26262ea5d5fe8cd03
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-03-16 16:45:57 +0000

    Added toasts, moved files around, added find by auth method

--- src/main/resources/static/js/user-auth/logout.js
+++ src/main/resources/static/js/user-auth/logout.js
@@ -1,16 +1,23 @@
+import {getUsernameFromAuth} from '../utils.js';
+
 export let clearAuth = async () => {
     let currentAuth = sessionStorage.getItem('auth');
+    currentAuth = currentAuth.replaceAll('"', '');
     let success;
+    sessionStorage.setItem('username', getUsernameFromAuth(currentAuth))
     await fetch(`user/logout/${currentAuth}`, {
         method: 'POST'
     }).then((response)=> {
         success = response.text();
     });
-    if (success === "true") {
-        console.log('Successfully Logged Out!');
-    } else {
-        console.log('Unsuccessful Logout Try Again!')
-    }
+    // TODO fix logout bug
+    console.log(success);
+    // if (success == 'true') {
+    //     console.log('Successfully Logged Out!');
+    // } else {
+    //     console.log('Unsuccessful Logout Try Again!')
+    // }
     sessionStorage.removeItem('auth');
+    sessionStorage.setItem('justLoggedOut', 'true');
     location.reload();
 }
