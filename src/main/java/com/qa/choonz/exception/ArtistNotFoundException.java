3dbf65f1193d955a857979407384218442d80191
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-03-17 14:48:11 +0000

    fixed exceptions unique serial ID and created new custom exception for user

--- src/main/java/com/qa/choonz/exception/ArtistNotFoundException.java
+++ src/main/java/com/qa/choonz/exception/ArtistNotFoundException.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Artist not found")
 public class ArtistNotFoundException extends PersistenceException {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8447598503082115460L;
 
 }
--- src/main/java/com/qa/choonz/exception/PlaylistNotFoundException.java
+++ src/main/java/com/qa/choonz/exception/PlaylistNotFoundException.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Playlist not found")
 public class PlaylistNotFoundException extends PersistenceException {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3980981986435610141L;
 
 }
