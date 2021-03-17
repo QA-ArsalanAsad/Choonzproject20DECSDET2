ccc3886e5253aa65ff73bbbde5117c6e0d0b2854
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-03-17 09:59:17 +0000

    Updated card builder and exposed H2 to TCP server, updated H2 pom.xml

--- src/main/resources/static/js/page-builders/artist/artist-card-builder.js
+++ src/main/resources/static/js/page-builders/artist/artist-card-builder.js
@@ -1,6 +1,6 @@
 export let artistCardBuilder =(artist)=>{
     let card = document.createElement('div');
-    card.className = 'card bg-dark regular-text';
+    card.className = 'card bg-dark regular-text border border-light pb-3 mb-5';
     let cardHeader = document.createElement('div');
     cardHeader.className = 'card-header';
     cardHeader.innerHTML = artist['name'];
