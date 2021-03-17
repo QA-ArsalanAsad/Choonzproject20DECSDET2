e3179be4c045401b0fc841cb23850b1408bbeaac
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-03-17 10:24:20 +0000

    Fixed the OneToOne relationship

--- src/main/java/com/qa/choonz/persistence/domain/Playlist.java
+++ src/main/java/com/qa/choonz/persistence/domain/Playlist.java
@@ -2,14 +2,7 @@ package com.qa.choonz.persistence.domain;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -43,7 +36,7 @@ public class Playlist {
 	@OneToMany(mappedBy = "playlist",  cascade = CascadeType.ALL)
 	private List<Playlist_Track> tracks;
 
-	@OneToOne
+	@ManyToOne
 	private User user;
 
 	public Playlist(long id, @NotNull @Size(max = 100) String name, @NotNull @Size(max = 500) String description,
