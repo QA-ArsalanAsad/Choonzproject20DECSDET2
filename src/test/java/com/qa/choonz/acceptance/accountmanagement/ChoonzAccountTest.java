cf01a470824545553bbe8c0a0b4356da3c5c6c7e
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-03-18 11:57:01 +0000

    Added playlist feature file

--- src/test/java/com/qa/choonz/acceptance/accountmanagement/ChoonzAccountTest.java
+++ src/test/java/com/qa/choonz/acceptance/accountmanagement/ChoonzAccountTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/AccountManagement",
+        features = "src/test/resources/features/AccountManagement.feature",
         glue = "com.qa.choonz.acceptance.accountmanagement.stepdefs"
 )
 
